f8d4f23bad5be18967b85fcb36fb03509edc74f5
GiorgiKhutsishvili <giorgi.khutsishvili04@geolab.edu.ge>
2018-01-31 15:54:57 +0400

    #3 - ფრონტის დაკავშირება ბექთან (2)

---
+++ sxva-saqartvelo-back-end/Controllers/DeveloperProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sxva_saqartvelo_back_end.Controllers
+{
+    public class DeveloperProfileController : Controller
+    {
+        // GET: DeveloperProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
