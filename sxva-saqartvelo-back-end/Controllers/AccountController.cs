45de9876552abf27d6ad2e5a6426f487a98ebd23
Anano Aspanidze <anano.aspanidze@gmail.com>
2018-02-02 06:06:02 +0400

    მივაბი ბაზა

--- sxva-saqartvelo-back-end/Controllers/AccountController.cs
+++ sxva-saqartvelo-back-end/Controllers/AccountController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sxva_saqartvelo_back_end.Models;
 
 namespace sxva_saqartvelo_back_end.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+
+        OtherGeorgiaEntities _db = new OtherGeorgiaEntities();
 
         public ActionResult Login()
         {
--- sxva-saqartvelo-back-end/Controllers/DevelopersController.cs
+++ sxva-saqartvelo-back-end/Controllers/DevelopersController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sxva_saqartvelo_back_end.Models;
 
 namespace sxva_saqartvelo_back_end.Controllers
 {
     public class DevelopersController : Controller
     {
-        // GET: Developers
+
+        OtherGeorgiaEntities _db = new OtherGeorgiaEntities();
+
         public ActionResult Index()
         {
             return View();
