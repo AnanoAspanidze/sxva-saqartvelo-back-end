b7a10c02be84991ee355e6d8a46d4cd2da29dd8b
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2018-02-15 17:16:59 +0400

    ცვლილებები რეგისტრაციის გვერდზე (2)

---
+++ sxva-saqartvelo-back-end/Scripts/MouseOverPassMouseOutPass.js
@@ -0,0 +1,37 @@
+﻿function mouseoverPass(obj) {
+    var obj = document.getElementById('Password');
+    obj.type = "text";
+}
+function mouseoutPass(obj) {
+    var obj = document.getElementById('Password');
+    obj.type = "password";
+}
+
+function mouseoverHover(obj) {
+    var obj = document.getElementById('RepeatPassword');
+    obj.type = "text";
+}
+function mouseoutHover(obj) {
+    var obj = document.getElementById('RepeatPassword');
+    obj.type = "password";
+}
+
+
+//show hide icon
+$(function () {
+    $('.showHidePd').hover(function () {
+        $('.showHidePd').attr('src', '/img/logos/if_visibility_password.svg');
+    },
+    function () {
+        $('.showHidePd').attr('src', '/img/logos/if_visibility-off.svg');
+    })
+});
+
+$(function () {
+    $('.showHideRePd').hover(function () {
+        $('.showHideRePd').attr('src', '/img/logos/if_visibility_password.svg');
+    },
+    function () {
+        $('.showHideRePd').attr('src', '/img/logos/if_visibility-off.svg');
+    })
+});
