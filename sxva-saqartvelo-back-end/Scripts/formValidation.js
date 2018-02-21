aaaa153293f4761b3114a53d15476c6e2d8242b2
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2018-02-21 17:30:06 +0400

    #9 - დეველოპერების შიდა გვერდის ცვლილებები (2)

---
+++ sxva-saqartvelo-back-end/Scripts/formValidation.js
@@ -0,0 +1,38 @@
+﻿function validateForm() {
+
+    if ($("#Name").val() == ""
+         || $("#Surname").val() == ""
+          || $("#Field").val() == ""
+           || $("Email").val() == ""
+            || $("#Password").val() == ""
+             || $("#RepeatPassword").val() == ""
+              || $("#Mobile").val() == "") {
+
+        $("#Name").css("border-color", "#e80c4d");
+        $("#Surname").css("border-color", "#e80c4d");
+        $("#Field").css("border-color", "#e80c4d");
+        $("#Email").css("border-color", "#e80c4d");
+        $("#Password").css("border-color", "#e80c4d");
+        $("#RepeatPassword").css("border-color", "#e80c4d");
+        $("#Mobile").css("border-color", "#e80c4d");
+    }
+
+
+
+    if ($("#Name").val() != null ||
+        $("#Surname").val() != null ||
+         $("#Field").val() != null ||
+           $("#Email").val() != null ||
+            $("#Password").val() != null ||
+             $("#RepeatPassword").val() != null ||
+              $("#Mobile").val() != null) {
+
+        $("input").keyup(function () {
+            var $this = $(this);
+            var insertedVal = $this.val();
+            if (insertedVal != null) {
+                $this.css("border-color", "");
+            }
+        });
+    }
+}
