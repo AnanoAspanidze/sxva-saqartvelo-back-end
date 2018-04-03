ee86439c8cddfe2a5e71b131a8bec2c1d9f19f71
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2018-04-03 17:48:42 +0400

    #14 - აარჩიე ადმინკა და გააკეთე ადმინის ლოგინი (1)

---
+++ sxva-saqartvelo-back-end/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sxva_saqartvelo_back_end.Models;
+using sxva_saqartvelo_back_end.Helpers;
+using sxva_saqartvelo_back_end.Filters;
+
+namespace sxva_saqartvelo_back_end.Controllers
+{
+    [AdminFilter]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
--- sxva-saqartvelo-back-end/Filters/AdminFilter.cs
+++ sxva-saqartvelo-back-end/Filters/AdminFilter.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using sxva_saqartvelo_back_end.Models;
+
 
 namespace sxva_saqartvelo_back_end.Filters
 {
-    public class AdminFilter
+    public class AdminFilter : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var admin = (Admin)filterContext.HttpContext.Session["admin"];
+
+            if (admin == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    { "controller", "Admin" },
+                    { "action", "Login"}
+                });
+            }
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
