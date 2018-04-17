c39daf522d6fb6c4917488ec8daa3f190ab043a5
GiorgiKhutsishvili <giorgi.khutsishvili04@geolab.edu.ge>
2018-04-18 00:21:31 +0400

    #15 - მეილის გაგზავნის ფუნქციონალი (1)

--- sxva-saqartvelo-back-end/Controllers/HomeController.cs
+++ sxva-saqartvelo-back-end/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using sxva_saqartvelo_back_end.Models;
 using PagedList.Mvc;
 using PagedList;
+using System.Web.Helpers;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace sxva_saqartvelo_back_end.Controllers
 {
@@ -31,6 +34,50 @@ namespace sxva_saqartvelo_back_end.Controllers
 
         public ActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Contact(SendMailModel SendEmail)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                email.From = new System.Net.Mail.MailAddress(SendEmail.Email);
+                email.To.Add("giorgi.khutsishvili04@geolab.edu.ge");
+                email.Subject = SendEmail.Subject;
+                email.Body = SendEmail.Body;
+
+
+                System.Net.Mail.SmtpClient SmtpServer = new System.Net.Mail.SmtpClient("smtp.gmail.com");
+                //WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 465;
+                WebMail.SmtpUseDefaultCredentials = true;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "giorgi.khutsishvili04@geolab.edu.ge";
+                WebMail.Password = "Geo12341234";
+                SmtpServer.Send(email);
+                ViewBag.SendSucceeded = "თქვენი შეტყობინება გაიგზავნა წარმატებით";
+              
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}",
+                                                validationError.PropertyName,
+                                                validationError.ErrorMessage);
+                    }
+                }
+            }
+            //catch(Exception)
+            //{
+            //    ViewBag.SendFailed = "შეტყობინების გაგზავნის დროს მოხდა შეცდომა";
+            //}
             return View();
         }
     }
---
+++ sxva-saqartvelo-back-end/Models/SendMailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace sxva_saqartvelo_back_end.Models
+{
+    public class SendMailModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "გთხოვთ, ჩაწერეთ თქვენი ელ.ფოსტა")]
+        [StringLength(50, ErrorMessage = "ჩაწერილი ელ.ფოსტა არ უნდა აღემატებოდეს 50 სიმბოლოს")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                    @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                    @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                    ErrorMessage = "გთხოვთ ჩაწეროთ სწორედ ელ.ფოსტის მისამართ")]
+        [EmailAddress(ErrorMessage = "ელ.ფოსტის ფორმატი არასწორია")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "ჩაწერეთ სათაური")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "ჩაწერეთ ტექსტი")]
+        public string Body { get; set; }
+    }
+}
